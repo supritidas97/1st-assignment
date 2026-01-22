d21f31f89de07ca62588d1f07e6305c0093b356d
Supriti Das <supritidas97@gmail.com>
2026-01-22 13:58:01 +0530

    Add files via upload

---
+++ mathematical_operations.py
@@ -0,0 +1,14 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+addition = int(num1) + int(num2)
+print("Addition: ", addition)
+
+subtraction = int(num1) - int(num2)
+print("Subtraction: ", subtraction)
+
+multiplication = int(num1) * int(num2)
+print("Multiplication: ", multiplication)
+
+division = int(num1) / int(num2)
+print("Division: ", division)
